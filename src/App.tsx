7b2029486cf34744f68ab1651e1948c2d08c879d
TeoDevGerman <38500085+TeoDevGerman@users.noreply.github.com>
2025-05-26 23:33:18 +0200

    delete: unneccessary Links

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import 'leaflet/dist/leaflet.css';
 
 
 const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const Projects = lazy(() => import("./pages/Projects"));
-const Contact = lazy(() => import("./pages/Contact"));
 const AnimatedSVG = lazy(() => import("./pages/AnimSVG"));
 function App() {
   return (
@@ -20,17 +17,6 @@ function App() {
           <Suspense fallback={<div>Loading...</div>}>
             <AnimatedSVG />
           </Suspense>} />
-        <Route path="/about" element={
-          <Suspense fallback={<div>Loading...</div>}>
-            <About />
-          </Suspense>} />
-        <Route path="/projects" element={
-          <Suspense fallback={<div>Loading...</div>}>
-            <Projects />
-          </Suspense>} />
-        <Route path="/contact" element={<Suspense fallback={<div>Loading...</div>}>
-          <Contact />
-        </Suspense>} />
       </Routes>
     </Router>
   );
